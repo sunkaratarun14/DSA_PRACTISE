b8186069f8bca0cb2c123fb4f7dba444f89f543d
sunkaratarun14 <23a95a6114@aec.edu.in>
2025-05-17 17:23:10 +0530

    commit

--- src/LinkedList/SinglyLL/LL.java
+++ src/LinkedList/SinglyLL/LL.java
@@ -4,5 +4,62 @@ import org.w3c.dom.Node;
 
 public class LL {
 
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public LL() {
+        this.size = 0;
     }
+
+    public void display(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.val +"->");
+            temp=temp.next;
+        }
+        System.out.print("end");
+
+    }
+    public void inserAtFirst(int val){
+        if (tail==null){
+            tail=head;
+        }
+        Node node = new Node(val);
+        node.next=head;
+        head=node;
+        size++;
+    }
+    public void insertAtLast(int val){
+        Node node = new Node(val);
+        tail.next=node;
+        tail=node;
+        if (tail==null){
+            inserAtFirst(val);
+        }
+        size++;
+
+
+    }
+    public void insertAtPOS(){}
+    public void deleteAtFirst(){}
+    public void deleteAtLast(){}
+    public void deleteAtPOS(){}
+
+    private class Node{
+        private int val;
+        private Node next;
+
+//        constructor
+        public Node(int val){
+            this.val=val;
+        }
+
+        public Node(int val, Node next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
 }
--- src/LinkedList/SinglyLL/Main.java
+++ src/LinkedList/SinglyLL/Main.java
@@ -2,6 +2,13 @@ package LinkedList.SinglyLL;
 
 public class Main {
     public static void main(String args[]){
-        System.out.println("werdfdf");
+        LL list = new LL();
+        list.inserAtFirst(10);
+        list.inserAtFirst(20);
+        list.inserAtFirst(30);
+        list.inserAtFirst(40);
+        list.inserAtFirst(50);
+        list.insertAtLast(99);
+        list.display();
     }
 }
